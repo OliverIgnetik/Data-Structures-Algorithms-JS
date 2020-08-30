f0d329314cac28c05457f6de929c697ef32ac423
Oliver Ignetik <olli@ozemail.com.au>
2020-08-30 15:06:54 +1000

    simple finding nemo function

--- BigO/asymptotic_analysis.js
+++ BigO/asymptotic_analysis.js
@@ -2,3 +2,16 @@
 
 // 1. Readable
 // 2. Scalable
+
+// Simple example
+const array = ['nemo'];
+
+function findNemo(array) {
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+}
+
+findNemo(array);
